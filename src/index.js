fae688ddc458696eeba1877b1ff1549230b8c390
Jason <jason.rostar@yahoo.com>
2020-07-23 14:21:41 -0700

    added login and register schema components

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger'
 import thunk from 'redux-thunk';
 
-const store = createStore(reducer, applyMiddleware(thunk, logger)); 
+const store = createStore(applyMiddleware(thunk, logger)); 
 
 ReactDOM.render(
   <Provider store={store}>
