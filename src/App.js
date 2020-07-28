78295bb25018005b2e4409de67c09ebdf61f79f9
Chris <chrisharwell72@gmail.com>
2020-07-27 20:08:52 -0400

    added useHistory.push() to login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { workoutAction } from "./state/actions/index.js";
-import Workouts from "./components/banner/Workouts.js";
+import Workouts from "./components/workout-form/Workouts.js";
 import "./App.css";
 import Footer from "./components/footer/footer"
 import PrivateRoute from "./utils/PrivateRoute.js";
@@ -9,6 +9,7 @@ import Header from "./components/header/header";
 import { Route } from "react-router-dom";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
+import { WorkoutClasses } from "./components/classes/Workout-Classes.js";
 
 function App(props) {
 
@@ -20,8 +21,7 @@ function App(props) {
       <br/>
       <br/>
   
-      <PrivateRoute exact path="/workouts">
-        {"//TODO add path based on API url"}
+      <PrivateRoute exact path="/classes">
         <Workouts workouts={props.storeProps} />
       </PrivateRoute>
 
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,64 +3,66 @@ import * as yup from "yup";
 import loginSchema from "../../validation/loginSchema";
 import axios from "axios";
 import "./login.styles.scss";
-import FormInput from "../form-input/form-input"
+import FormInput from "../form-input/form-input";
 import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
 
 function Login(props) {
+  let { push } = useHistory();
+
   const [buttonDisabled, setButtonDisabled] = useState(true);
 
- 
   const [formState, setFormState] = useState({
     username: "",
-    password: ""
+    password: "",
   });
   const [errors, setErrors] = useState({
     username: "",
-    password: ""
+    password: "",
   });
 
   useEffect(() => {
-    loginSchema.isValid(formState).then(valid => {
+    loginSchema.isValid(formState).then((valid) => {
       setButtonDisabled(!valid);
     });
   }, [formState]);
 
-  const formSubmit = e => {
+  const formSubmit = (e) => {
     e.preventDefault();
     axios
       .post("auth/login", formState)
-      .then(res => {
+      .then((res) => {
         window.localStorage.setItem("token", res.data.token);
         localStorage.setItem("id", res.data.id);
         props.setLoggedState(true);
-       
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
+    push("/");
   };
 
-  const inputChange = e => {
+  const inputChange = (e) => {
     e.persist();
     const newFormData = {
       ...formState,
       [e.target.name]:
-        e.target.type === "checkbox" ? e.target.checked : e.target.value
+        e.target.type === "checkbox" ? e.target.checked : e.target.value,
     };
 
     yup
       .reach(loginSchema, e.target.name)
       .validate(e.target.value)
-      .then(valid => {
+      .then((valid) => {
         setErrors({
           ...errors,
-          [e.target.name]: ""
+          [e.target.name]: "",
         });
       })
-      .catch(err => {
+      .catch((err) => {
         setErrors({
           ...errors,
-          [e.target.name]: err.errors[0]
+          [e.target.name]: err.errors[0],
         });
       });
 
@@ -69,45 +71,41 @@ function Login(props) {
 
   return (
     <div className="sign-up">
-    <h1 className="title">Login</h1>
-    <br/>
-    <form className="login-form" onSubmit={formSubmit}>
-   
-       
-    
-          {errors.username.length > 0 ? (
-            <p style={{ color: "red" }}>{errors.username}</p>
-          ) : null}
-          <FormInput
-            type="text"
-            name="username"
-            label="Username"
-            value={formState.username}
-            inputChange={inputChange}
-          />
-      
+      <h1 className="title">Login</h1>
+      <br />
+      <form className="login-form" onSubmit={formSubmit}>
+        {errors.username.length > 0 ? (
+          <p style={{ color: "red" }}>{errors.username}</p>
+        ) : null}
+        <FormInput
+          type="text"
+          name="username"
+          label="Username"
+          value={formState.username}
+          inputChange={inputChange}
+        />
+
         <br />
         <br />
-      
-          {errors.password.length > 0 ? (
-            <p style={{ color: "red" }}>{errors.password}</p>
-          ) : null}
-          <FormInput
-            type="password"
-            name="password"
-            label="Password"
-            value={formState.password}
-            inputChange={inputChange}
-          />
-      
+
+        {errors.password.length > 0 ? (
+          <p style={{ color: "red" }}>{errors.password}</p>
+        ) : null}
+        <FormInput
+          type="password"
+          name="password"
+          label="Password"
+          value={formState.password}
+          inputChange={inputChange}
+        />
+
         <br />
         <br />
-        
-    </form>
-    <Button onClick={formSubmit} disabled={buttonDisabled}>
-          Login
-        </Button>
-</div>
+      </form>
+      <Button onClick={formSubmit} disabled={buttonDisabled}>
+        Login
+      </Button>
+    </div>
   );
 }
 
