c96111498ee320981fe02aa73d95e579dd1b0ed1
Chris <chrisharwell72@gmail.com>
2020-07-28 19:32:01 -0400

    added buttons for edit and deleting a workout

--- src/components/classes/Workout-Classes.js
+++ src/components/classes/Workout-Classes.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { axiosWithAuth } from "../../utils/axiosWithAuth";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Card, CardContent, Typography, Button } from "@material-ui/core";
 
 const WorkoutClasses = (props) => {
   return (
@@ -12,13 +12,11 @@ const WorkoutClasses = (props) => {
               <Typography variant="h5" gutterBottom>
                 Workout: {workout.name}
               </Typography>
-              <Typography>
-                  Duration: {workout.duration} minutes
-              </Typography>
-              <Typography>
-                  Type: {workout.type}
-              </Typography>
+              <Typography>Duration: {workout.duration} minutes</Typography>
+              <Typography>Type: {workout.type}</Typography>
             </CardContent>
+            <Button variant="outlined" color="primary">Edit</Button> // TODO add onClick for buttons as well as routing
+            <Button variant="outlined" color="secondary">Delete</Button>
           </Card>
         ))}
       </div>
