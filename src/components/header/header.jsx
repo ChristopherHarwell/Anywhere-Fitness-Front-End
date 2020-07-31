fb07095b032b7e623f9aa97882e7ae99b7890471
Jason <jason.rostar@yahoo.com>
2020-07-31 11:11:27 -0700

    added styles to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,39 +1,37 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { Button } from "@material-ui/core";
-
 import "./header-styles.scss";
 
 function Header() {
   return (
-    <div className="header">
-     
+    <div className="background-image">
       
+
+
+<div className="header">
     <div className="nav-buttons">
-          <Button 
-          variant="outlined" 
-          color="primary"
-          >
-            <Link 
+          <Button variant="outlined" color="primary">
+            <NavLink 
             className="nav-button" 
             to="/login"
             >
             Login
-            </Link>
+            </NavLink>
           </Button>
           <br/>
-      
           <Button variant="outlined" color="primary">
             <Link className="nav-button" to="/Register">Register</Link>
           </Button>
     </div>
+
 <NavLink className="header-title" to="/">
-
          <h1>anywhere-fitness</h1>
-          
+         <br/>
           </NavLink>
-
     </div>
+    <h5 className="second-title">Providing diverse fitness classes for anyone, anywhere, anytime.</h5>
+</div>
   );
 }
 
