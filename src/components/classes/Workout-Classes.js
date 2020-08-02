bedb0ad2ab9b402d2758bf901d658a782991aac9
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-08-01 22:32:22 -0700

    search class created

--- src/components/classes/Workout-Classes.js
+++ src/components/classes/Workout-Classes.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, CardContent, Typography, Button } from "@material-ui/core";
-
+import searchClass from "../searchClass";
+import axios from "axios";
 
 //TODO add the rest of the workout data
 const WorkoutClasses = (props) => {
+  const [classes, setClasses] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://anywhere-fitness-3.herokuapp.com/api/classes")
+      .then((res) => {
+        console.log(res.data);
+        setClasses(res.data);
+      });
+  }, []);
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const result = classes.filter((cls) => {
+    return cls.title.toLowerCase().includes(search);
+  });
+
+  
   return (
     <>
       <div>
+        <center>
+          <searchClass value={search} onChange={handleChange} />
+        </center>
         {props.classes.map((workout) => (
           <Card>
             <CardContent>
@@ -15,13 +40,12 @@ const WorkoutClasses = (props) => {
               </Typography>
               <Typography>Duration: {workout.duration} minutes</Typography>
               <Typography>Type: {workout.type}</Typography>
-        <Typography>Start Date: {workout.date}</Typography>
+              <Typography>Start Date: {workout.date}</Typography>
             </CardContent>
 
-              <Button onClick={props.edit} variant="outlined" color="primary">
-                Edit
-              </Button>
-
+            <Button onClick={props.edit} variant="outlined" color="primary">
+              Edit
+            </Button>
 
             <Button onClick={props.delete} variant="outlined" color="secondary">
               Delete
@@ -32,6 +56,5 @@ const WorkoutClasses = (props) => {
     </>
   );
 };
-
 
 export default WorkoutClasses;
